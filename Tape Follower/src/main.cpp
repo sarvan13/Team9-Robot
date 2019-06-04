1d39f4df044534c09c3dd7e3a43a3e8df0c19c52
alexcarbo <alexcarbo6@gmail.com>
2019-06-04 16:42:33 -0700

    Code For Tape follower to move for 5 seconds then stop. Also outputs readings of IR sensors during this time

--- Tape Follower/src/main.cpp
+++ Tape Follower/src/main.cpp
@@ -1,9 +1,62 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <time.h>
+#include <math.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define LEFT_SENSOR PA_4
+#define RIGHT_SENSOR PA_5
+#define LEFT_FORWARD_PIN PA_6
+#define LEFT_REVERSE_PIN PA_7
+#define RIGHT_FORWARD_PIN PB_1 
+#define RIGHT_REVERSE_PIN PB_0
+#define TPWM 500
+#define CLOCKF 100000
+#define DIFTHRESH 5
+
+float previousLeft = 0.0;
+float previousRight = 0.0;
+int count = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LEFT_SENSOR, INPUT_ANALOG);
+  pinMode(RIGHT_SENSOR, INPUT_ANALOG);
+  pinMode(LEFT_FORWARD_PIN, OUTPUT);
+  pinMode(LEFT_REVERSE_PIN, OUTPUT);
+  pinMode(RIGHT_FORWARD_PIN, OUTPUT);
+  pinMode(RIGHT_REVERSE_PIN, OUTPUT);
+
+  //start going forwards
+  pwm_start(LEFT_FORWARD_PIN, CLOCKF, TPWM, 200, 1);
+  pwm_start(RIGHT_FORWARD_PIN, CLOCKF, TPWM, 200, 1);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop()
+{
+    if(count == 5) {
+      pwm_start(LEFT_FORWARD_PIN, CLOCKF, TPWM, 0, 1);
+      pwm_start(RIGHT_FORWARD_PIN, CLOCKF, TPWM, 0, 1);
+    }
+
+    float leftSensor = analogRead(LEFT_SENSOR);
+    float rightSensor = analogRead(RIGHT_SENSOR);
+
+
+    Serial.println("Left: "); Serial.println(leftSensor);
+    Serial.println("Right: "); Serial.println(rightSensor);
+
+    if (abs(leftSensor - previousLeft) > DIFTHRESH || abs(rightSensor - previousRight) > DIFTHRESH) {
+        // somehow adjust the speed of both motors to turn a little bit to the needed side
+        
+        previousLeft = leftSensor;
+        previousRight = rightSensor;
+    } 
+    delay(1000);
+
+    count++;
 }
