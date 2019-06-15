e6fa920f7ed4b7602fb43682ea7c4193b04d0f5c
alexcarbo <alexcarbo6@gmail.com>
2019-06-14 19:00:59 -0700

    Something

--- Tape Follower/src/main.cpp
+++ Tape Follower/src/main.cpp
@@ -3,18 +3,24 @@
 #include <time.h>
 #include <math.h>
 
-#define LEFT_SENSOR PB_0
-#define RIGHT_SENSOR PB_1
-#define LEFT_FORWARD_PIN PA_6
-#define LEFT_REVERSE_PIN PA_7
-#define RIGHT_FORWARD_PIN PA_4 
-#define RIGHT_REVERSE_PIN PA_5
+#define LEFT_SENSOR PA3
+#define RIGHT_SENSOR PA2
+#define RIGHT_REVERSE_PIN PA_7
+#define RIGHT_FORWARD_PIN PA_6
+#define LEFT_REVERSE_PIN PB_1 
+#define LEFT_FORWARD_PIN PB_0
 #define TPWM 500
 #define CLOCKF 100000
-#define DIFTHRESH 5
 
-float previousLeft = 0.0;
-float previousRight = 0.0;
+#define DIFTHRESH 100
+#define REG_SPEED 125
+
+#define KP 25
+#define KD 0
+
+float previousError = 0;
+float error = 0.0;
+
 int count = 0;
 
 void setup()
@@ -22,41 +28,57 @@ void setup()
   Serial.begin(115200);
 
   // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LEFT_SENSOR, INPUT_ANALOG);
-  pinMode(RIGHT_SENSOR, INPUT_ANALOG);
   pinMode(LEFT_FORWARD_PIN, OUTPUT);
   pinMode(LEFT_REVERSE_PIN, OUTPUT);
   pinMode(RIGHT_FORWARD_PIN, OUTPUT);
   pinMode(RIGHT_REVERSE_PIN, OUTPUT);
+  pinMode(LEFT_SENSOR, INPUT_ANALOG);
+  pinMode(RIGHT_SENSOR, INPUT_ANALOG);
 
   //start going forwards
-  // pwm_start(LEFT_FORWARD_PIN, CLOCKF, TPWM, 200, 1);
-  // pwm_start(RIGHT_FORWARD_PIN, CLOCKF, TPWM, 200, 1);
+  // digitalWrite(LEFT_FORWARD_PIN, HIGH);
+  pwm_start(LEFT_FORWARD_PIN, CLOCKF, TPWM, 70, 1);
+  pwm_start(RIGHT_FORWARD_PIN, CLOCKF, TPWM, 70, 1);
 }
-
 
 void loop()
 {
-    // if(count == 5) {
-    //   pwm_start(LEFT_FORWARD_PIN, CLOCKF, TPWM, 0, 1);
-    //   pwm_start(RIGHT_FORWARD_PIN, CLOCKF, TPWM, 0, 1);
-    // }
-
     float leftSensor = analogRead(LEFT_SENSOR);
     float rightSensor = analogRead(RIGHT_SENSOR);
+    float previousError = error;
 
+    // Serial.println("Left: "); Serial.println(leftSensor);
+    // Serial.println("Right: "); Serial.println(rightSensor);
 
-    Serial.println("Left: "); Serial.println(leftSensor);
-    Serial.println("Right: "); Serial.println(rightSensor);
+    if (rightSensor < DIFTHRESH && leftSensor < DIFTHRESH) {
+        if (previousError < 0){
+            error = -5;
+        } else {
+            error = 5;
+        }
+    } else if (leftSensor < DIFTHRESH){
+      error = 1;
+    } else if (rightSensor < DIFTHRESH) {
+      error = -1;
+    } else {
+      error = 0;
+    }
 
-    if (abs(leftSensor - previousLeft) > DIFTHRESH || abs(rightSensor - previousRight) > DIFTHRESH) {
-        // somehow adjust the speed of both motors to turn a little bit to the needed side
-        
-        previousLeft = leftSensor;
-        previousRight = rightSensor;
-    } 
-    delay(1000);
+    float p = KP * error;
+    float d = KD * (error - previousError);
+    // float d = 0;
+    float g = p + d;
 
-    count++;
+    if(error > 0){
+      pwm_start(LEFT_FORWARD_PIN, CLOCKF, TPWM, REG_SPEED + g, 0);
+      pwm_start(RIGHT_FORWARD_PIN, CLOCKF, TPWM, REG_SPEED, 0);
+    }else if(error < 0){
+      pwm_start(LEFT_FORWARD_PIN, CLOCKF, TPWM, REG_SPEED, 0);
+      pwm_start(RIGHT_FORWARD_PIN, CLOCKF, TPWM, REG_SPEED - g, 0);
+    }else {
+      pwm_start(LEFT_FORWARD_PIN, CLOCKF, TPWM, REG_SPEED, 0);
+      pwm_start(RIGHT_FORWARD_PIN, CLOCKF, TPWM, REG_SPEED, 0);
+    }
+    Serial.print("Error: ");
+    Serial.println(error);
 }
